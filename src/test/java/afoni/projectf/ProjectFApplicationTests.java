5a25cc3add64e09dcc68c9538e70afb890d2ae02
afoni <afoninvitalya3@gmail.com>
2026-02-18 08:19:55 +0300

    Инициализация проекта

---
+++ src/test/java/afoni/projectf/ProjectFApplicationTests.java
@@ -0,0 +1,13 @@
+package afoni.projectf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
